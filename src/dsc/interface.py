731df487887e2bd89517d4153449b78f7cc0c686
Gao Wang <gaow@uchicago.edu>
2016-03-16 02:00:40 -0500

    Continue to work on DSC to SoS jobs

--- src/dsc/interface.py
+++ src/dsc/interface.py
@@ -11,6 +11,12 @@ from dsc import PACKAGE, VERSION
 from workhorse import execute, show, submit
 
 def main():
+    def add_common_args(obj):
+        obj.add_argument('-v', '--verbosity', type = int, choices = list(range(5)), default = '2',
+                         help='''Output error (0), warning (1), info (2), debug (3) and trace (4)
+                         information.''')
+        obj.add_argument('--debug', action='store_true', help = argparse.SUPPRESS)
+    #
     parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument('--version', action = 'version', version = '{} {}'.format(PACKAGE, VERSION))
     subparsers = parser.add_subparsers(dest = 'subcommands')
@@ -18,12 +24,11 @@ def main():
     p = subparsers.add_parser('execute', help = 'Execute DSC',
                               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('dsc_file', metavar = "dsc_file", help = 'DSC file')
-    p.add_argument('-v', '--verbosity', type = int, choices = list(range(5)), default = '2',
-                   help='''Output error (0), warning (1), info (2), debug (3) and trace (4)
-                   information.''')
+    add_common_args(p)
     p.set_defaults(func = execute)
     p = subparsers.add_parser('show', help = 'Explore DSC benchmark data',
                               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    add_common_args(p)
     p.set_defaults(func = show)
     args, argv = parser.parse_known_args()
     env.verbosity = args.verbosity
