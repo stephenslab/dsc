1456186a65d37b7cc603c47c7b1bc2b44bc33c2a
Gao Wang <gaow@uchicago.edu>
2018-02-25 11:04:36 -0600

    Use seed for python example

--- vignettes/one_sample_location_python/PY/scenarios/rnorm.py
+++ vignettes/one_sample_location_python/PY/scenarios/rnorm.py
@@ -1,3 +1,4 @@
 # produces n random numbers from normal with specified mean
 import numpy
+numpy.random.seed(seed)
 x=numpy.random.normal(loc=true_mean, size=n)
--- vignettes/one_sample_location_python/PY/scenarios/rt.py
+++ vignettes/one_sample_location_python/PY/scenarios/rt.py
@@ -1,3 +1,4 @@
 # produces n random numbers from t with df=2 and  with specified mean
 import numpy
+numpy.random.seed(seed)
 x=true_mean+numpy.random.standard_t(df=2,size=n)
