176107d2de6b559b0d7045b006941e0da5927336
Gao Wang <gaow@uchicago.edu>
2019-06-18 16:46:37 -0500

    Bump SoS version requirement (INFO field fix)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name        = "dsc",
       cmdclass    = cmdclass,
       package_dir = {'dsc': 'src'},
       install_requires = ['numpy', 'pandas>=0.24.1', 'sympy', 'numexpr',
-                          'sos>=0.19.11', 'sos-pbs>=0.19.6', 'h5py', 'PTable',
+                          'sos>=0.19.13', 'sos-pbs>=0.19.6', 'h5py', 'PTable',
                           'pyarrow>=0.5.0', 'sqlalchemy', 'tzlocal',
                           'msgpack-python']
       )
